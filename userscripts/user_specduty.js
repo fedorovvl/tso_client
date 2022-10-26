48ee86d3382ca1b93133454b966a4e667f8aa036
Vladimir Fedorov <sin@krasno.ru>
2022-10-26 19:22:44 +0300

    refactoring

--- userscripts/user_specduty.js
+++ userscripts/user_specduty.js
@@ -1,27 +1,23 @@
-
-
-const _exudspecDutyLang = [ 
-								[ ["en-uk"] , 
-									[
-										[ "menuItemName", "Spec Duty" ] ,
-										[ "menuTitle", "Specialists duty time" ] ,
-										[ "ColumnEstimated", "Estimated" ] ,
-										[ "ColumnArrival", "Arrival" ] ,
-										[ "NoData", "No data found!" ] 
-									]
-								] ,
-								[ ["pt-br"] , 
-									[
-										[ "menuItemName", "Tarefas Ativas" ] ,
-										[ "menuTitle", "Tempo das tarefas dos especialistas" ] ,
-										[ "ColumnEstimated", "Estimado" ] ,
-										[ "ColumnArrival", "Chegada" ] ,
-										[ "NoData", "Nenhuma tarefa encontrada!" ] 
-									]
-								]
-						];
-						
-						
+const _exudspecDutyLang = { 
+	"en-uk": {
+		"menuItemName": "Spec Duty"  ,
+		"menuTitle": "Specialists duty time"  ,
+		"ColumnEstimated": "Estimated"  ,
+		"ColumnArrival": "Arrival"  ,
+		"NoData": "No data found!" 
+	},
+	"pt-br": {
+		"menuItemName": "Tarefas Ativas"  ,
+		"menuTitle": "Tempo das tarefas dos especialistas"  ,
+		"ColumnEstimated": "Estimado"  ,
+		"ColumnArrival": "Chegada"  ,
+		"NoData": "Nenhuma tarefa encontrada!" 
+	}
+};
+var _exudSpecDutyType = 1; // 1 = explorer 2 = geologist 3 = generals
+var _exudDutySPECIALIST_TYPE = swmmo.getDefinitionByName("Enums::SPECIALIST_TYPE");
+var _exudBtnToSpecType = { 'dutyExplorersBtn': 1,'dutyGeologistBtn': 2,'dutyGeneralsBtn': 3 };
+var idL = loca.getSelectedLanguage();			
 addToolsMenuItem(_exudspecDutyGetLabel("menuItemName"), specDutyTime);
 
 function specDutyTime(event) {
@@ -29,149 +25,75 @@ function specDutyTime(event) {
 	$("div[role='dialog']:not(#dutyModal):visible").modal("hide");
 	// create modal
 	createModalWindow('dutyModal', _exudspecDutyGetLabel("menuTitle"));
-	// change data id
-	$("#dutyModal .modal-body").attr('id', 'dutyModalData');
+	if($('#dutyModal .modal-footer .dutyExplorersBtn').length == 0)
+	{
+		$("#dutyModal .modal-footer").prepend([
+			$('<button>').attr({ "id": "dutyExplorersBtn", "class": "btn btn-primary pull-left dutyExplorersBtn" }).text(loca.GetText("SPE", "Explorer")),
+			$('<button>').attr({ "id": "dutyGeologistBtn", "class": "btn btn-primary pull-left dutyGeologistBtn" }).text(loca.GetText("SPE", "Geologist")),
+			$('<button>').attr({ "id": "dutyGeneralsBtn", "class": "btn btn-primary pull-left dutyGeneralsBtn" }).text(loca.GetText("SPE", "General")),
+		]);
+		$('button.dutyExplorersBtn, button.dutyGeologistBtn, button.dutyGeneralsBtn').click(function(event) {
+			_exudSpecDutyType = _exudBtnToSpecType[this.id];
+			$('#_exudDutyResultDiv').html(dutyGetData());
+		});
+	}
 	// fill modal data 
-	dutySetMainWindow();
+	$('#dutyModalData').html('<div class="container-fluid"><div id="_exudDutyResultDiv">'+dutyGetData()+'</div></div>');
 	// show modal
 	$('#dutyModal:not(:visible)').modal({ backdrop: "static" });
 
 }
 
-var _exudSpecDutyType = 1; // 1 = explorer 2 = geologist 3 = generals
-var _exudDutySPECIALIST_TYPE = swmmo.getDefinitionByName("Enums::SPECIALIST_TYPE");
-
 function _exudspecDutyGetLabel(id)
 {
-	
-	var idL = loca.getSelectedLanguage();
-	var ls = _exudspecDutyGetLanguage(idL);
-	
-	var y = 0;
-
-	for(y = 0 ; y < ls.length ; y++)
-		if (ls[y][0] == id)
-			return ls[y][1];
-
-	return "RES not found : " + id;
+	if(!_exudspecDutyLang[idL] && !_exudspecDutyLang["en-uk"][id]) { return "RES not found : " + id; }
+	return _exudspecDutyLang[idL] && _exudspecDutyLang[idL][id] ? _exudspecDutyLang[idL][id] : _exudspecDutyLang["en-uk"][id];
 }	
 
-function _exudspecDutyGetLanguage(id)
-{
-	
-	var y = 0;
-	for(y = 0 ; y < _exudspecDutyLang.length ; y++)
-		if (_exudspecDutyLang[y][0] == id)
-			return _exudspecDutyLang[y][1];
-		
-	return _exudspecDutyLang[0][1]; // English default
-}
-
-
-function dutySetMainWindow()
-{
-	out = '<div class="container-fluid"><button type="button" id="dutyExplorersBtn" class="btn btn-primary dutyExplorersBtn">'+loca.GetText("SPE", "Explorer")+'</button>';
-	out += ' <button type="button" class="btn btn-primary dutyGeologistBtn">'+loca.GetText("SPE", "Geologist")+'</button>';
-	out += ' <button type="button" class="btn btn-primary dutyGeneralsBtn">'+loca.GetText("SPE", "General")+'</button>';
-	out += '<br/>';	
-	out += '<div id="_exudDutyResultDiv"></div></div>';
-
-	
-	$('#dutyModalData').html(out);
-
-	$('#dutyModalData .dutyExplorersBtn').click(dutySelectExplorers);
-	$('#dutyModalData .dutyGeologistBtn').click(dutySelectGeologists);
-	$('#dutyModalData .dutyGeneralsBtn').click(dutySelectGenerals);
-
-}
-
-function dutySelectExplorers()
-{
-	$('#dutyModalData .dutyExplorersBtn').html("<b>"+loca.GetText("SPE", "Explorer")+"</b>");
-	$('#dutyModalData .dutyGeologistBtn').html(loca.GetText("SPE", "Geologist"));
-	$('#dutyModalData .dutyGeneralsBtn').html(loca.GetText("SPE", "General"));
-	_exudSpecDutyType = 1;
-	$('#_exudDutyResultDiv').html(dutyGetData());
-}
-
-function dutySelectGeologists()
-{
-	$('#dutyModalData .dutyExplorersBtn').html(loca.GetText("SPE", "Explorer"));
-	$('#dutyModalData .dutyGeologistBtn').html("<b>"+loca.GetText("SPE", "Geologist")+"</b>");
-	$('#dutyModalData .dutyGeneralsBtn').html(loca.GetText("SPE", "General"));
-	
-	_exudSpecDutyType = 2;
-	$('#_exudDutyResultDiv').html(dutyGetData());
-}
-
-function dutySelectGenerals()
-{
-	$('#dutyModalData .dutyExplorersBtn').html(loca.GetText("SPE", "Explorer"));
-	$('#dutyModalData .dutyGeologistBtn').html(loca.GetText("SPE", "Geologist"));
-	$('#dutyModalData .dutyGeneralsBtn').html("<b>"+loca.GetText("SPE", "General")+"</b>");
-	_exudSpecDutyType = 3;
-	$('#_exudDutyResultDiv').html(dutyGetData());
-}
-
 function dutyGetData() {
-	out = '<div><div class="row"><div class="col-xs-6 col-sm-6 col-lg-6"  style="background-color:gray;height:23px">'+loca.GetText("LAB","Name")+'</div><div class="col-xs-3 col-sm-3 col-lg-3"  style="background-color:gray;height:23px">'+_exudspecDutyGetLabel("ColumnEstimated")+'</div><div class="col-xs-3 col-sm-3 col-lg-3 massSend" style="background-color:gray;height:23px">'+_exudspecDutyGetLabel("ColumnArrival")+'</div></div>';
-	var GI = swmmo.application.mGameInterface;
-	var PlayerID = GI.mCurrentPlayer.GetPlayerId();
+	out = createTableRow([
+			[6, loca.GetText("LAB","Name")],
+			[3, _exudspecDutyGetLabel("ColumnEstimated")],
+			[3, _exudspecDutyGetLabel("ColumnArrival")]
+	], true);
+	var PlayerID = swmmo.application.mGameInterface.mCurrentPlayer.GetPlayerId();
 	isThereAnySpec = false;
 	var listSpec = new Array();
 	swmmo.application.mGameInterface.mCurrentPlayerZone.GetSpecialists_vector().sort(0).forEach(function(item){
-	var isValid = false;
-	var ItemName = item.getName(false);
-	
-		switch(_exudSpecDutyType) {
-			case 1:
-				isValid = (item.GetBaseType() == 1);
-				break;
-			case 2:
-				isValid = (item.GetBaseType() == 2);
-				break;
-			case 3:
-				var pid = item.getPlayerID();
-				isValid = (_exudDutySPECIALIST_TYPE.IsGeneral(item.GetType()) && (pid == PlayerID));
-				/** I see it later
-				if (isValid && (PlayerID != pid))
-				{
-					var pname = GI.GetPlayerName_string(pid);
-					if (pname != null)
-						ItemName += ' (' + pname + ')';
-				}
-				*/
-				break;
+		var isValid = item.GetBaseType() == _exudSpecDutyType || (_exudSpecDutyType == 3 && _exudDutySPECIALIST_TYPE.IsGeneral(item.GetType()));
+		if(item.GetTask() == null || !isValid) { return; }
+		var ItemName = item.getName(false);
+		if(_exudSpecDutyType == 3 && PlayerID != item.getPlayerID()) {
+			var pname = swmmo.application.mGameInterface.GetPlayerName_string(pid);
+			if (pname != null)
+				ItemName += ' (' + pname + ')';
 		}
-
-		try {
-		  if(isValid && (item.GetTask() != null)) {
-			  var i =  [ ItemName , item.GetTask().GetRemainingTime() ];
-			  listSpec.push( i  );
-			  isThereAnySpec = true;
-			
-		  }
-		} catch (e) {}
+		listSpec.push( [ ItemName , item.GetTask().GetRemainingTime() ]  );
+		isThereAnySpec = true;
 	});
 	
 	if(!isThereAnySpec)
-		return _exudspecDutyGetLabel("NoData");
+		return out + _exudspecDutyGetLabel("NoData");
 
 	listSpec.sort(dutyCompare);
 	
 	listSpec.forEach(function(item){
-			out += '<div class="row"><div class="col-xs-6 col-sm-6 col-lg-6 name">' + item[0] + '</div><div class="col-xs-3 col-sm-3 col-lg-3">'+loca.FormatDuration(item[1], 1)+'</div><div class="col-xs-3 col-sm-3 col-lg-3">' + dtf.format(new window.runtime.Date(Date.now() + item[1])) + '</div></div>';
+		out += createTableRow([
+			[6, item[0]],
+			[3, loca.FormatDuration(item[1], 1)],
+			[3, dtf.format(new window.runtime.Date(Date.now() + item[1]))]
+		]);
 	});
 	
-	return out + '</div>';
+	return out;
 }
 
 function dutyCompare(a, b)
 {
 	try{
-	if (a[1] > b[1]) return 1;
-	if (a[1] < b[1]) return -1;
-	return a[0].toLowerCase().localCompare(b[0].toLowerCase());
+		if (a[1] > b[1]) return 1;
+		if (a[1] < b[1]) return -1;
+		return a[0].toLowerCase().localCompare(b[0].toLowerCase());
 	}
 	catch (e) {}
 	return 0;
