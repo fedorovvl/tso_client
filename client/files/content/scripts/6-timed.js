b2f77203ed0adb5b05b7e92f7c2a6523c6e61d03
Vladimir Fedorov <sin@krasno.ru>
2022-11-11 16:02:48 +0300

    remove queue, open on select

--- client/files/content/scripts/6-timed.js
+++ client/files/content/scripts/6-timed.js
@@ -32,66 +32,48 @@ function TimedMenuHandler(event)
 			}
 			var estimate = calcTotalTime(queue);
 			out += createTableRow([
-				[4, $('<button>', { 
-					'class': 'close',
-					'style': 'float: left;',
-					'value': type,
-					'data-toggle': "collapse",
-					'data-target': "#collapse" + type,
-					'aria-expanded': "false",
-					'aria-controls': "collapse" + type
-				}).html('<span style="color:#fff;">&#8645;&nbsp;&nbsp;</span>').prop('outerHTML') + loca.GetText("BUI", queue.productionBuilding.GetBuildingName_string())],
+				[4, $('<span>', { 'id': queue.productionBuilding.GetGrid() }).hide().prop('outerHTML') + loca.GetText("BUI", queue.productionBuilding.GetBuildingName_string())],
 				[2, queue.mTimedProductions_vector.length],
 				[3, estimate > 0 ? loca.FormatDuration(estimate, 1) : '-'],
-				[3, estimate > 0 ? dtf.format(new window.runtime.Date(Date.now() + calcTotalTime(queue))) : '-']
+				[3, estimate > 0 ? dtf.format(new window.runtime.Date(Date.now() + estimate)) : '-']
 			]);
-			out += '<div class="collapse" id="collapse'+type+'"><div class="container-fluid">';
-			out += createTableRow([
-				[4, loca.GetText("LAB", "Name")],
-				[2, loca.GetText("QUL", "Adv_EoWEventQ2")],
-				[3, getText('prod_estimate')],
-				[3, getText('prod_finish')]
-			], true);
-			var timeByOrder = 0;
-			queue.mTimedProductions_vector.forEach(function(item) {
-				timeByOrder += calcTime(item);
-				out += createTableRow([
-					[4, loca.GetText("RES", item.GetType())],
-					[2, item.GetProducedItems() + '/' + item.GetAmount()],
-					[3, loca.FormatDuration(timeByOrder, 1)],
-					[3, dtf.format(new window.runtime.Date(Date.now() + timeByOrder))]
-				]);
-			});
-			out += '</div></div>';
 		});
 		out += '</div>';
 		$("#timedModalData").html(out);
+		$("#timedModalData .container-fluid .row:gt(0)").click(timedRowClick);
+		$("#timedModalData .container-fluid .row:gt(0) div:first-child").css('cursor', 'pointer');
 		$('#timedModal:not(:visible)').modal({backdrop: "static"});
 	} catch(e) { 
 		TimedMenuHandler(null);
 	}
 }
 
+function timedRowClick(event)
+{
+	try
+	{
+		var grid = $(event.target).children('span')[0].id;
+		var bui = swmmo.application.mGameInterface.mCurrentPlayerZone.mStreetDataMap.GetBuildingByGridPos(grid);
+		swmmo.application.mGameInterface.SelectBuilding(bui);
+		$('#timedModal .btnClose').trigger('click');
+	} catch(e) {}
+}
+
 function getCultureInfo(queue)
 {
 	var cooldown = queue.productionBuilding.getRemainingCooldown();
 	return createTableRow([
-		[4, "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + loca.GetText("BUI", queue.productionBuilding.GetBuildingName_string())],
-		[5, cooldown <= 0 ? '-' : "{0}: {1}".format(loca.GetText("LAB", "cooldown"), loca.FormatDuration(cooldown, 1)), 'text-center'],
-		[3, cooldown <= 0 ? '-' : dtf.format(new window.runtime.Date(Date.now() + cooldown)) ]
+		[4, $('<span>', { 'id': queue.productionBuilding.GetGrid() }).hide().prop('outerHTML') + loca.GetText("BUI", queue.productionBuilding.GetBuildingName_string())],
+		[5, cooldown <= 0 ? '' : "{0}: {1}".format(loca.GetText("LAB", "cooldown"), loca.FormatDuration(cooldown, 1)), 'text-center'],
+		[3, cooldown <= 0 ? '' : dtf.format(new window.runtime.Date(Date.now() + cooldown)) ]
 	]);
-}
-
-function calcTime(item)
-{
-	return (((item.GetAmount() - item.GetProducedItems()) * item.GetProductionTime()) - item.GetCollectedTime()) / item.GetProductionOrder().GetTimeBonus();
 }
 
 function calcTotalTime(queue)
 {
 	var result = 0;
 	queue.mTimedProductions_vector.forEach(function(item) {
-		result += calcTime(item);
+		result += (((item.GetAmount() - item.GetProducedItems()) * item.GetProductionTime()) - item.GetCollectedTime()) / item.GetProductionOrder().GetTimeBonus();
 	});
 	return result;
 }
