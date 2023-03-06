3bd20c0d46c42c493c196fd1f42b19284ba1577f
Vladimir Fedorov <sin@krasno.ru>
2023-03-06 12:28:12 +0300

    workflow debug

--- build.bat
+++ build.bat
@@ -1,9 +1,6 @@
-@echo off
-echo %cd%
-dir
 msbuild client.sln /p:Configuration=Release /p:Platform=x86 /t:Clean,Build
-rmdir \S "client\files\content\Adobe AIR"
-xcopy client\files\runtime_x64\ client\files\content\ /Y /S
+rmdir \S "%cd%\client\files\content\Adobe AIR"
+xcopy %cd%\client\files\runtime_x64\ %cd%\client\files\content\ /Y /S
 msbuild client.sln /p:Configuration=Release /p:Platform=x86 /t:Clean,Build /p:AssemblyName=client_x64
-rmdir \S "client\files\content\Adobe AIR"
-xcopy client\files\runtime_x86\ client\files\content\ /Y /S
+rmdir \S "%cd%\client\files\content\Adobe AIR"
+xcopy %cd%\client\files\runtime_x86\ %cd%\client\files\content\ /Y /S
