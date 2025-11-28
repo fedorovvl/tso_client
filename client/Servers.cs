5fb92504a3502bc433dfce3aa72e9d820414cdda
Vladimir Fedorov <sin@krasno.ru>
2025-11-28 11:52:02 +0300

    fix mx auth

--- client/Servers.cs
+++ client/Servers.cs
@@ -21,7 +21,7 @@ namespace client
             { "ru", new Server(){ domain = "https://www.thesettlersonline.ru", uplay = "/ru/api/user/uplay", main = "/ru/%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0", play = "/ru/play" } },
             { "pl", new Server(){ domain = "https://www.thesettlersonline.pl", uplay = "/pl/api/user/uplay", main = "/pl/strona-g%C5%82%C3%B3wna", play = "/pl/graj" } },
             { "es2", new Server(){ domain = "https://www.thesettlersonline.es", uplay = "/es/api/user/uplay", main = "/es/p%C3%A1gina-de-inicio", play = "/es/jugar" } },
-            { "es", new Server(){ domain = "https://juego-thesettlersonline.com", uplay = "/es/api/user/uplay", main = "/es/p%C3%A1gina-de-inicio", play = "/es/jugar" } },
+            { "es", new Server(){ domain = "https://www.juego-thesettlersonline.com", uplay = "/es/api/user/uplay", main = "/es/p%C3%A1gina-de-inicio", play = "/es/jugar" } },
             { "nl", new Server(){ domain = "https://www.thesettlersonline.nl", uplay = "/nl/api/user/uplay", main = "/nl/homepage", play = "/nl/play" } },
             { "cz", new Server(){ domain = "https://www.thesettlersonline.cz", uplay = "/cz/api/user/uplay", main = "/cs/domovsk%C3%A1-str%C3%A1nka", play = "/cs/play" } },
             { "pt", new Server(){ domain = "https://www.thesettlersonline.com.br", uplay = "/pt/api/user/uplay", main = "/pt/p%C3%A1gina-inicial", play = "/pt/jogar" } },
