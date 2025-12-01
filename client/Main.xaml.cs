27f60440cd6d16410a3dcdec145802017c8d3710
Vladimir Fedorov <sin@krasno.ru>
2025-12-01 11:59:36 +0300

    x64 arg revert

--- client/Main.xaml.cs
+++ client/Main.xaml.cs
@@ -612,7 +612,7 @@ namespace client
                 extraVersion = extraVersion != string.Format("#{0}#", "TESTTAG") ? "-" + extraVersion : "";
                 if (debug)
                     File.AppendAllText("debug.txt", "start tso with " + _settings.tsoArg + "\r\n");
-                System.Diagnostics.Process.Start(string.Format("{0}\\client{1}.exe", ClientDirectory, _settings.x64 ? "64" : ""), string.Format("{0}&version={1}{2}", _settings.tsoArg, appversion, extraVersion));
+                System.Diagnostics.Process.Start(string.Format("{0}\\client{1}.exe", ClientDirectory, _settings.x64 || cmd["x64"] != null ? "64" : ""), string.Format("{0}&version={1}{2}", _settings.tsoArg, appversion, extraVersion));
             } catch
             {
                 MessageBox.Show(string.Format("{0}\\META-INF\\AIR\\application.xml", ClientDirectory) + " corrupted.. Remove it and try again");
